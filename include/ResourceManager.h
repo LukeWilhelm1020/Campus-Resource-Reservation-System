c20ad8748c27dd35e86beda95a7d21700112fd55
FallenCV03 <joseangelverdin1@gmail.com>
2026-09-19 22:06:28 -0500

    Implementation of resource manager header

---
+++ include/ResourceManager.h
@@ -0,0 +1,24 @@
+#ifndef RESOURCE_MANAGER_H
+#define RESOURCE_MANAGER_H
+
+#include "Resource.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class ResourceManager {
+  public:
+    ResourceManager (); //default constructor to create an empty resource manager
+
+    bool loadResources (string filename); //getting the information from the text file
+
+    void displayResources() const; // displays the resources currently saved in the text file
+
+    void displayAvailability() const; // displays if the resources are available or not
+  private:
+    vector<Resource> resources; //contains all resources loaded from the resource data file
+
+};
+
+#endif
